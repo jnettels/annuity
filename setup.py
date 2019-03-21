972e619215f03d1995050b5c06e887b503757308
Joris Nettelstroth <joris.nettelstroth@stw.de>
2019-03-21 17:38:18 +0100

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+VDI2067
+=======
+Setup for the package ``VDI2067``, based on the German VDI 2067:
+
+    **VDI 2067, Part 1**
+
+    **Economic efficiency of building installations**
+
+    **Fundamentals and economic calculation**
+
+    *September 2012 (ICS 91.140.01)*
+
+To install, run the following command:
+
+.. code::
+
+    python setup.py install
+
+You can then import the module with:
+
+.. code::
+
+    import VDI2067
+
+"""
+
+from distutils.core import setup
+
+setup(name='VDI2067',
+      version='0.1.0',
+      description='VDI 2067: Economic efficiency of building installations',
+      author='Joris Nettelstroth',
+      py_modules=['VDI2067'],
+      )
