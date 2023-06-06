b28997fdc6a39a967f8505b290721c1f0e65344d
Joris Nettelstroth <joris@nettelstroth.de>
2023-06-06 08:12:28 +0200

    Avoid unnecessary imports

--- annuity/__init__.py
+++ annuity/__init__.py
@@ -1,4 +1,3 @@
-from setuptools_scm import get_version
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
@@ -8,10 +7,11 @@ try:
 except DistributionNotFound:
     try:
         # If package is not installed, try to get version from git
+        from setuptools_scm import get_version
         __version__ = get_version(version_scheme='post-release',
                                   root='..',
                                   relative_to=__file__)
-    except LookupError as e:
+    except (LookupError, Exception) as e:
         print(e)
         __version__ = '0.0.0'
 
